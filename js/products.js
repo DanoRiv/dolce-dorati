851bdb2d0cc2f3270aec508da5ebfc945d09db5c
DanoRiv <danio260496@gmail.com>
2024-11-19 23:35:35 -0500

    product list

---
+++ js/products.js
@@ -0,0 +1,56 @@
+async function load() {
+  try {
+    const response = await fetch('./assets/data.json')
+    let myData = await response.json()
+
+    const container = document.getElementById('products-data')
+  
+    myData.forEach(item => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('products--card');
+
+
+      productCard.innerHTML = `
+        <img src="${item.img}" alt="cake picture" class="product-card--picture">
+        <p>${item.name}</p>
+        <div class="product-card--addBtn">
+          <span>${item.price}</span>
+          <a href="#" data-id="${item.id}">
+            <img src="../assets/icons/add btn.svg" alt="add to cart icon">
+          </a>
+        </div>
+      `;
+
+      container.appendChild(productCard);
+    });
+
+    document.querySelectorAll('.product-card--addBtn a').forEach(btn=> {
+      btn.addEventListener('click', addToCart);
+    })
+  }catch(err) {
+    console.log(err)
+  }
+}
+
+function addToCart(event){
+  event.preventDefault();
+  const productId = event.currentTarget.dataset.id;
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+  if(!cart.includes(productId)){
+    cart.push(productId);
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  updateCartCount();
+}
+
+function updateCartCount(){
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  document.getElementById('cart-count').textContent = cart.length;
+}
+
+window.onload = ()=> {
+  load();
+  updateCartCount();
+}
